9a50e2a9cdd00dd746e7c1be8af37027b5457471
cre4tive97 <harryborrison5148@gmail.com>
2021-10-06 05:40:26 +0900

    Quasar, Composition API setted

---
+++ src/pages/TestVue.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <q-btn
+      color="primary"
+      icon="check"
+      label="OK"
+      @click="onClick"
+    />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TestVue',
+
+  setup () {
+    //
+  }
+})
+</script>
